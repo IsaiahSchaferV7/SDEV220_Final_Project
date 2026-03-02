c17d410f2184e0298516519e9e80f5e0477f7d32
IsaiahSchaferV7 <schoolschafer@gmail.com>
2026-03-02 18:16:21 -0500

    Add files via upload

--- PygameProject/SDEV220_Pygame_Snake.py
+++ PygameProject/SDEV220_Pygame_Snake.py
@@ -1,6 +1,5 @@
 import pygame
 from Apple import respawn_apple # type: ignore
-from PlayerCharacter import Player # type: ignore
 
 pygame.init()
 
@@ -10,55 +9,55 @@ CELL_SIZE = 20
 
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
-FIRST_GREY = (128, 128, 128)
-SECOND_GREY = (200, 200, 200)
+GREEN = (0, 255, 0)
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-
-class draw_board():
-    screen.fill((FIRST_GREY))
-    for x in range(0, screen_width - 20, CELL_SIZE):
-        pygame.draw.line(screen, SECOND_GREY, (x, 0), (x, screen_height))
-    for y in range(0, screen_height - 20, CELL_SIZE):
-        pygame.draw.line(screen, SECOND_GREY, (0, y), (screen_width, y))
-    pygame.display.update()
-
-border_thickness = 30
-my_rect = (20, 20, 980, 780)
-
-player = Player(440, 340)
 clock = pygame.time.Clock()
-player_speed_per_second = 300
+
+my_rect = (20, 20, 980, 780)
 apple_rect = respawn_apple()
-all_sprites = pygame.sprite.Group(player)
+
+player_SPS = 300
+head_pos = pygame.Vector2(100, 100)
+player_seg = [pygame.Rect(head_pos.x, head_pos.y, 20, 20)]
+p_direction = pygame.Vector2(player_SPS, 0)
+segment_limit = 1
 
 run = True
 while run:
+    dt = clock.tick(60) / 1000.0
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    all_sprites.draw(screen)
-    pygame.draw.rect(screen, BLACK, my_rect, border_thickness)
-    pygame.draw.rect(screen, (255, 0, 0), apple_rect)
-    dt = clock.tick(60) / 1000.0
+        if event.type == pygame.KEYDOWN:
+            if event.key in (pygame.K_LEFT, pygame.K_a) and p_direction.x == 0:
+                p_direction = pygame.Vector2(-player_SPS, 0)
+            elif event.key in (pygame.K_RIGHT, pygame.K_d) and p_direction.x == 0:
+                p_direction = pygame.Vector2(player_SPS, 0)
+            elif event.key in (pygame.K_UP, pygame.K_w) and p_direction.y == 0:
+                p_direction = pygame.Vector2(0, -player_SPS)
+            elif event.key in (pygame.K_DOWN, pygame.K_s) and p_direction.y == 0:
+                p_direction = pygame.Vector2(0, player_SPS)        
+    screen.fill(BLACK)
+    
+    head_pos += p_direction * dt
+    new_segment = pygame.Rect(head_pos.x, head_pos.y, 20,20)
+    if new_segment != player_seg[-1]:
+        player_seg.append(new_segment)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        player.rect.x -= player_speed_per_second * dt
-    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        player.rect.x += player_speed_per_second * dt
-    if keys[pygame.K_UP] or keys[pygame.K_w]:
-        player.rect.y -= player_speed_per_second * dt
-    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        player.rect.y += player_speed_per_second * dt
-
-    player.update()
-
-    if player.rect.colliderect(apple_rect):
+    if player_seg[-1].colliderect(apple_rect):
         apple_rect = respawn_apple()
         pygame.draw.rect(screen, (255, 0, 0), apple_rect)
-    # Ensure apple does not spawn on player
+        segment_limit += 10
+    
+    while len(player_seg) > segment_limit:
+        player_seg.pop(0)
 
+    for seg in player_seg:
+        pygame.draw.rect(screen, GREEN, seg)
+
+    pygame.draw.rect(screen, (255, 0, 0), apple_rect)
     pygame.display.update()
 
     tick: (60)
